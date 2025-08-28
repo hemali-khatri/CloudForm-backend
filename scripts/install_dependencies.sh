6edac804b6b5ba00f6c153353db180fbdb04d220
Hemali Khatri <hemali.khatri@tatvasoft.com>
2025-08-28 17:50:25 +0530

    changed to new

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 cd /home/ec2-user/backend-app
 
-echo "Installing Node.js..."
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+APP_DIR="/home/ec2-user/backend-app"
+cd "$APP_DIR"
 
+# Install Node.js 16.x (LTS version for Amazon Linux 2)
+echo "Installing Node.js 18..."
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
 sudo yum install -y nodejs
+
+# Fix file ownership and permissions
+echo "Fixing permissions..."
+sudo chown -R ec2-user:ec2-user "$APP_DIR"
 
 # Install dependencies
 echo "Installing project dependencies..."
