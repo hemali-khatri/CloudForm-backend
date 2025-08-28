fadbe536ced46d02fba6ece47f8663a3d02f52a0
Hemali Khatri <hemali.khatri@tatvasoft.com>
2025-08-28 17:29:40 +0530

    changed version to 15

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,8 +4,8 @@ APP_DIR="/home/ec2-user/backend-app"
 cd "$APP_DIR"
 
 # Install Node.js 16.x (LTS version for Amazon Linux 2)
-echo "Installing Node.js 18..."
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
+echo "Installing Node.js 16..."
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
 sudo yum install -y nodejs
 
 # Fix file ownership and permissions
