c44676a363e8e7bca64926d96f24d6f787ab3569
Hemali Khatri <hemali.khatri@tatvasoft.com>
2025-09-03 15:35:28 +0530

    changes done

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,25 @@
 module.exports = {
-  development: {
+  // development: {
+  //   client: 'mysql2',
+  //   connection: {
+  //     host: '127.0.0.1',
+  //     user: 'root',
+  //     password: '',
+  //     database: 'blog_db'
+  //   },
+  //   migrations: {
+  //     directory: './migrations'
+  //   },
+  //   seeds: {
+  //     directory: './seeds'
+  //   }
+  development:{
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: '',
-      database: 'blog_db'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
     },
     migrations: {
       directory: './migrations'
