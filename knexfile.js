2cf9c58941e805bda5142673f7cb16abf552a2aa
Hemali Khatri <hemali.khatri@tatvasoft.com>
2025-09-03 16:57:22 +0530

    done changes

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,25 @@
+require('dotenv').config();
 module.exports = {
-  development: {
+  // development: {
+  //   client: 'mysql2',
+  //   connection: {
+  //     host: '127.0.0.1',
+  //     user: 'root',
+  //     password: 'Tatva@123',
+  //     database: 'finalproject'
+  //   },
+  //   migrations: {
+  //     directory: './migrations'
+  //   },
+  //   seeds: {
+  //     directory: './seeds'
+  //   }
+  development:{
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'Tatva@123',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
       database: 'finalproject'
     },
     migrations: {
@@ -13,20 +28,6 @@ module.exports = {
     seeds: {
       directory: './seeds'
     }
-  // development:{
-  //   client: 'mysql2',
-  //   connection: {
-  //     host: process.env.DB_HOST,
-  //     user: process.env.DB_USER,
-  //     password: process.env.DB_PASS,
-  //     database: process.env.DB_NAME
-  //   },
-  //   migrations: {
-  //     directory: './migrations'
-  //   },
-  //   seeds: {
-  //     directory: './seeds'
-  //   }
   },
 
   production: {
@@ -34,8 +35,8 @@ module.exports = {
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
+      password: process.env.DB_PASSWORD,
+      database: 'finalproject'
     },
     migrations: {
       directory: './migrations'
