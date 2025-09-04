3c5152a447199494d8fbf2bf94af50705b9c70e6
Hemali Khatri <hemali.khatri@tatvasoft.com>
2025-09-04 14:09:55 +0530

    changes done

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -4,6 +4,6 @@ echo "Stopping Node.js backend server..."
 
 # Find and kill any process running server.js
 pkill -f "node backendreact.js" || echo "No backendreact.js process found."
-pm2 delete backend-app || echo "No PM2 process named backend-app found."
+# pm2 delete backend-app || echo "No PM2 process named backend-app found."
 
 echo "Server stopped."
